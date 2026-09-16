f66a95bb829d07b2bc8063f35b2e2907da393b4a
KangDaHeee <dhk3161@gmail.com>
2026-09-16 18:36:19 +0900

    ✨CWinApp 상속 및 재정의

--- G26W0202Minimum/main.cpp
+++ G26W0202Minimum/main.cpp
@@ -1,2 +1,15 @@
-#include <afxwin.h>
-CWinApp app;
+﻿#include <afxwin.h>
+//CWinApp app;
+
+class MyApp : public CWinApp {
+public:
+	virtual BOOL InitInstance();
+};
+
+BOOL MyApp::InitInstance() {
+	AfxMessageBox(L"파생 클래스의 InitInstance() 재정의");
+
+	return TRUE;
+}
+
+MyApp app;
